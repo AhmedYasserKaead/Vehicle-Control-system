13c44ba48bd7e15c33f72229ba23b57636a779f8
Ahmed Kaead <85291999+AhmedYasserKaead@users.noreply.github.com>
2022-08-19 18:02:10 +0200

    Add files via upload

---
+++ datatypes.h
@@ -0,0 +1,27 @@
+/*
+ * datatypes.h
+ *
+ *  Created on: Aug 19, 2022
+ *      Author: Ahmed-PC
+ */
+
+#ifndef DATATYPES_H_
+#define DATATYPES_H_
+
+typedef unsigned char u_int8;
+typedef signed char s_int8;
+typedef unsigned short u_int16;
+typedef signed short s_int16;
+typedef unsigned long u_int32;
+typedef signed long s_int32;
+typedef unsigned long long u_int64;
+typedef signed long long s_int64;
+typedef float f_32;
+typedef double f_64;
+
+
+
+
+
+
+#endif /* DATATYPES_H_ */
---
+++ first_question.h
@@ -0,0 +1,13 @@
+/*
+ * first_question.h
+ *
+ *  Created on: Aug 19, 2022
+ *      Author: Ahmed-PC
+ */
+
+#ifndef FIRST_QUESTION_H_
+#define FIRST_QUESTION_H_
+
+u_int8 first_question();
+
+#endif /* FIRST_QUESTION_H_ */
